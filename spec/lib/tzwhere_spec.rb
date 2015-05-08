e1b9685dfb3ba4ff890c1fb37ea455c3e7e8cd54
Andreas Eger <andreas.eger@runtastic.com>
2015-05-08 14:40:09 +0200

    add some more specs

--- spec/lib/tzwhere_spec.rb
+++ spec/lib/tzwhere_spec.rb
@@ -1,7 +1,19 @@
 describe TZWhere do
-  it "can make lookups" do
-    expect(TZWhere.lookup(43.18333333, -79.233333)).to eq(ActiveSupport::TimeZone.new("America/Toronto"))
-    expect(TZWhere.lookup(41.7599, -87.2079)).to eq(ActiveSupport::TimeZone.new("America/Chicago"))
+  [
+    [51.51002454, -0.11930466, "Europe/London"],       # UK
+    [43.18333333, -79.233333, "America/Toronto"],
+    [41.7599, -87.2079, "America/Chicago"],
+    [48.2442964, 14.23535228, "Europe/Vienna"],        # Austria
+    [35.46066995, 100.45898438, "Asia/Shanghai"],      # China
+    [28.88315961, 80.38696289, "Asia/Kathmandu"],      # Nepal
+    [-8.0374734, -42.28637695, "America/Fortaleza"],   # Brazil
+    [-19.1451682, 46.49414063, "Indian/Antananarivo"], # Madagascar
+    [19.74602424, -155.46203613, "Pacific/Honolulu"],  # Hawaii
+    [-21.20745873, -175.17219543, "Pacific/Tongatapu"] # Tonga
+  ].each do |latitude, longitude, time_zone|
+    it "can make lookups in #{time_zone}" do
+      expect(TZWhere.lookup(latitude, longitude)).to eq(ActiveSupport::TimeZone.new(time_zone))
+    end
   end
 
   it "loads points only on first lookup" do
